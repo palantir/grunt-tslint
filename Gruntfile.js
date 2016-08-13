9184a98ab78cf0aa94a2e01bad8eae2cf0fbd1ce
Mike McCaughan <github@mikemccaughan.com>
2016-08-13 16:38:18 -0500

    Fixing line endings

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,57 +1,114 @@
 /*
+
  * Copyright 2013 Palantir Technologies, Inc.
+
  *
+
  * Licensed under the Apache License, Version 2.0 (the "License");
+
  * you may not use this file except in compliance with the License.
+
  * You may obtain a copy of the License at
+
  *
+
  *     http://www.apache.org/licenses/LICENSE-2.0
+
  *
+
  * Unless required by applicable law or agreed to in writing, software
+
  * distributed under the License is distributed on an "AS IS" BASIS,
+
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
  * See the License for the specific language governing permissions and
+
  * limitations under the License.
+
  */
+
+
 
 "use strict";
 
+
+
 module.exports = function (grunt) {
+
     grunt.loadNpmTasks("grunt-contrib-jshint");
+
     grunt.loadNpmTasks("grunt-contrib-clean");
 
+
+
     grunt.initConfig({
+
         jshint: {
+
             all: [
+
                 "Gruntfile.js",
+
                 "tasks/*.js"
+
             ],
+
             options: {
+
                 jshintrc: ".jshintrc"
+
             }
+
         },
 
+
+
         tslint: {
+
             errors: {
+
                 options: {
+
                     configuration: "tslint.json"
+
                 },
+
                 files: {
+
                     src: [
+
                         "test/fixtures/correctFile.ts",
+
                         "test/fixtures/errorFile1.ts",
+
                         "test/fixtures/errorFile2.ts"
+
                     ]
+
                 }
+
             }
+
         }
+
     });
 
+
+
     // actually load this plugin's task(s)
+
     grunt.loadTasks("tasks");
+
+
 
     grunt.registerTask("test", ["tslint"]);
 
+
+
     // by default, lint and run all tests
+
     grunt.registerTask("default", ["jshint", "test"]);
+
 };
+
--- tasks/tslint.js
+++ tasks/tslint.js
@@ -1,100 +1,200 @@
 /*
+
  * Copyright 2013 Palantir Technologies, Inc.
+
  *
+
  * Licensed under the Apache License, Version 2.0 (the "License");
+
  * you may not use this file except in compliance with the License.
+
  * You may obtain a copy of the License at
+
  *
+
  *     http://www.apache.org/licenses/LICENSE-2.0
+
  *
+
  * Unless required by applicable law or agreed to in writing, software
+
  * distributed under the License is distributed on an "AS IS" BASIS,
+
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
  * See the License for the specific language governing permissions and
+
  * limitations under the License.
+
  */
+
+
 
 "use strict";
 
+
+
 module.exports = function (grunt) {
+
     var Linter = require("tslint");
 
+
+
     grunt.registerMultiTask("tslint", "A linter for TypeScript.", function () {
+
         var options = this.options({
+
             configuration: null,
+
             formatter: "prose",
+
             outputFile: null,
+
             appendToOutput: false
+
         });
+
+
 
         var specifiedConfiguration = options.configuration;
 
+
+
         var done = this.async();
+
         var failed = 0;
 
+
+
         // Iterate over all specified file groups, async for 'streaming' output on large projects
+
         grunt.util.async.reduce(this.filesSrc, true, function (success, filepath, callback) {
+
             if (!grunt.file.exists(filepath)) {
+
                 grunt.log.warn('Source file "' + filepath + '" not found.');
+
             } else {
+
                 var configuration = specifiedConfiguration;
+
                 if (configuration == null || typeof configuration === "string") {
+
                     configuration = Linter.findConfiguration(configuration, filepath);
+
                 }
+
                 options.configuration = configuration;
 
+
+
                 var contents = grunt.file.read(filepath);
+
                 var linter = new Linter(filepath, contents, options);
+
                 var result = linter.lint();
 
+
+
                 if (result.failureCount > 0) {
+
                     var outputString = "";
+
                     var outputFile = options.outputFile;
+
                     var appendToOutput = options.appendToOutput;
+
+
 
                     failed += result.failureCount;
 
+
+
                     if (outputFile != null && grunt.file.exists(outputFile)) {
+
                         if (appendToOutput) {
+
                             outputString = grunt.file.read(outputFile);
+
                         } else {
+
                             grunt.file.delete(outputFile);
+
                         }
+
                     }
+
                     result.output.split("\n").forEach(function (line) {
+
                         if (line !== "") {
+
                             if (outputFile != null) {
+
                                 outputString += line + "\n";
+
                             } else {
+
                                 grunt.log.error(line);
+
                             }
+
                         }
+
                     });
+
                     if (outputFile != null) {
+
                         grunt.file.write(outputFile, outputString);
+
                     }
+
                     success = false;
+
                 }
+
             }
+
+
 
             // Using setTimout as process.nextTick() doesn't flush
+
             setTimeout(function () {
+
                 callback(null, success);
+
             }, 1);
 
+
+
         }, function (err, success) {
+
             if (err) {
+
                 done(err);
+
             } else if (success) {
+
                 var okMessage = this.filesSrc.length + " " + grunt.util.pluralize(this.filesSrc.length, "file/files") + " lint free.";
+
                 grunt.log.ok(okMessage);
+
                 done();
+
             } else {
+
                 var errorMessage = failed + " " + grunt.util.pluralize(failed, "error/errors") + " in " +
+
                     this.filesSrc.length + " " + grunt.util.pluralize(this.filesSrc.length, "file/files");
+
                 grunt.log.error(errorMessage);
+
                 done(false);
+
             }
+
         }.bind(this));
+
     });
+
 };
+
